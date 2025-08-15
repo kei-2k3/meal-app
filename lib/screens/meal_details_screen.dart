4f8c437014d7f1ebb5200ba804ded518d687e6d6
Thu Rain Hein <thurainhein.work@gmail.com>
2025-08-15 20:44:45 +0700

    Completed: Improving the meal detail screen.

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -10,11 +10,48 @@ class MealDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 14),
+            Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 14),
+            for (final ingredient in meal.ingredients)
+              Text(ingredient, style: TextStyle(color: Colors.white)),
+            SizedBox(height: 24),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 12,
+                ),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
