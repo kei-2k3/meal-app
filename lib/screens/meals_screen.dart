6295e1c31298cca953827096686998c704ce671c
Thu Rain Hein <thurainhein.work@gmail.com>
2025-08-15 17:32:47 +0700

    Completed: Loading meals data into a screen.

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -9,9 +9,38 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget body = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 16,
+        children: [
+          Text(
+            'Nothing here.',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+          Text(
+            'Try selecting a different category!.',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isEmpty) {
+      body = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) {
+          return Text(meals[index].title);
+        },
+      );
+    }
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: SizedBox(),
+      body: body,
     );
   }
 }
